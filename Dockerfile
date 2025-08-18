990a0b47c9f84a0fb97b958f50dd1b4de3622832
protheus6 <sauvage.jeff@gmail.com>
2025-08-18 17:27:18 +0200

    init

---
+++ Dockerfile
@@ -0,0 +1,65 @@
+# syntax=docker/dockerfile:1
+
+FROM tomcat:9-jdk11-temurin-jammy
+
+RUN apt-get update \
+    && apt-get upgrade -y
+RUN apt-get install -y \
+	aapt \
+	wget \
+	sed \
+        postgresql-client \
+	&& rm -rf /var/lib/apt/lists/*
+RUN mkdir -p /usr/local/tomcat/conf/Catalina/localhost
+RUN mkdir -p /usr/local/tomcat/ssl
+
+# Set to 1 to force updating the config files
+# If not set, they will be created only if there's no files
+#ENV FORCE_RECONFIGURE=true
+ENV FORCE_RECONFIGURE=
+
+# Available values: en, ru (en by default)
+ENV INSTALL_LANGUAGE=en
+
+#ENV ADMIN_EMAIL=
+
+# Different for open source and premium versions!
+ENV SHARED_SECRET=changeme-C3z9vi54
+
+ENV HMDM_VARIANT=os
+ENV DOWNLOAD_CREDENTIALS=
+ENV HMDM_URL=https://h-mdm.com/files/hmdm-5.30.3-$HMDM_VARIANT.war
+ENV CLIENT_VERSION=6.14
+
+ENV SQL_HOST=localhost
+ENV SQL_PORT=5432
+ENV SQL_BASE=hmdm
+ENV SQL_USER=hmdm
+ENV SQL_PASS=Ch@nGeMe
+
+ENV PROTOCOL=https
+#ENV BASE_DOMAIN=your-domain.com
+
+# Set this parameter to your local IP address 
+# if your server is behind the NAT
+#ENV LOCAL_IP=172.31.91.82
+
+# Comment it to use custom certificates
+ENV HTTPS_LETSENCRYPT=true
+# Mount the custom certificate path if custom certificates must be used
+# ENV_HTTPS_CERT_PATH is the path to certificates and keys inside the container
+#ENV HTTPS_CERT_PATH=/cert
+ENV HTTPS_CERT=cert.pem
+ENV HTTPS_FULLCHAIN=fullchain.pem
+ENV HTTPS_PRIVKEY=privkey.pem
+
+EXPOSE 8080
+EXPOSE 8443
+EXPOSE 31000
+
+COPY docker-entrypoint.sh /
+COPY update-web-app-docker.sh /opt/hmdm/
+COPY tomcat_conf/server.xml /usr/local/tomcat/conf/server.xml 
+ADD templates /opt/hmdm/templates/
+
+ENTRYPOINT ["/docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+HMDM_DIR=/opt/hmdm
+TEMPLATE_DIR=$HMDM_DIR/templates
+TOMCAT_DIR=/usr/local/tomcat
+BASE_DIR=$TOMCAT_DIR/work
+CACHE_DIR=$BASE_DIR/cache
+PASSWORD=123456
+
+for DIR in cache files plugins logs; do
+   [ -d "$BASE_DIR/$DIR" ] || mkdir "$BASE_DIR/$DIR"
+done
+
+if [ ! -z "$LOCAL_IP" ]; then
+    EXISTS=`grep $BASE_DOMAIN /etc/hosts`
+    if [ -z "$EXISTS" ] || [ "$FORCE_RECONFIGURE" = "true" ]; then
+        grep -v $BASE_DOMAIN /etc/hosts > /etc/hosts~
+	cp /etc/hosts~ /etc/hosts
+	echo "$LOCAL_IP $BASE_DOMAIN" >> /etc/hosts
+	rm -f /etc/hosts~
+    fi
+fi
+
+HMDM_WAR="$(basename -- $HMDM_URL)"
+
+if [ ! -f "$CACHE_DIR/$HMDM_WAR" ]; then
+    wget $DOWNLOAD_CREDENTIALS $HMDM_URL -O $CACHE_DIR/$HMDM_WAR
+fi
+
+if [ ! -f "$TOMCAT_DIR/webapps/ROOT.war" ] || [ "$FORCE_RECONFIGURE" = "true" ]; then
+    cp $CACHE_DIR/$HMDM_WAR $TOMCAT_DIR/webapps/ROOT.war
+fi
+
+$HMDM_DIR/update-web-app-docker.sh
+
+if [ ! -f "$BASE_DIR/log4j.xml" ] || [ "$FORCE_RECONFIGURE" = "true" ]; then
+    cp $TEMPLATE_DIR/conf/log4j_template.xml $BASE_DIR/log4j-hmdm.xml
+fi
+
+if [ ! -d "$BASE_DIR/emails" ] || [ "$FORCE_RECONFIGURE" = "true" ]; then
+    cp -r $TEMPLATE_DIR/emails $BASE_DIR/emails
+fi
+
+if [ ! -d $TOMCAT_DIR/conf/Catalina/localhost ]; then
+    mkdir -p $TOMCAT_DIR/conf/Catalina/localhost
+fi
+
+if [ ! -f "$TOMCAT_DIR/conf/Catalina/localhost/ROOT.xml" ] || [ "$FORCE_RECONFIGURE" = "true" ]; then
+    cat $TEMPLATE_DIR/conf/context_template.xml | sed "s|_SQL_HOST_|$SQL_HOST|g; s|_SQL_PORT_|$SQL_PORT|g; s|_SQL_BASE_|$SQL_BASE|g; s|_SQL_USER_|$SQL_USER|g; s|_SQL_PASS_|$SQL_PASS|g; s|_PROTOCOL_|$PROTOCOL|g; s|_BASE_DOMAIN_|$BASE_DOMAIN|g; s|_SHARED_SECRET_|$SHARED_SECRET|g;" > $TOMCAT_DIR/conf/Catalina/localhost/ROOT.xml 
+fi
+
+for DIR in cache files plugins logs; do
+   [ -d "$BASE_DIR/$DIR" ] || mkdir "$BASE_DIR/$DIR"
+done
+
+if [ "$INSTALL_LANGUAGE" != "ru" ]; then
+    INSTALL_LANGUAGE=en
+fi
+
+if [ ! -f "$BASE_DIR/init.sql" ] || [ "$FORCE_RECONFIGURE" = "true" ]; then
+    cat $TEMPLATE_DIR/sql/hmdm_init.$INSTALL_LANGUAGE.sql | sed "s|_ADMIN_EMAIL_|$ADMIN_EMAIL|g; s|_HMDM_VERSION_|$CLIENT_VERSION|g; s|_HMDM_VARIANT_|$HMDM_VARIANT|g" > $BASE_DIR/init1.sql
+fi
+
+FILES_TO_DOWNLOAD=$(grep https://h-mdm.com $BASE_DIR/init1.sql | awk '{ print $4 }' | sed "s/'//g; s/)//g; s/,//g")
+
+cat $BASE_DIR/init1.sql | sed "s|https://h-mdm.com|$PROTOCOL://$BASE_DOMAIN|g" > $BASE_DIR/init.sql
+rm $BASE_DIR/init1.sql
+
+cd $BASE_DIR/files
+for FILE in $FILES_TO_DOWNLOAD; do
+    FILENAME=$(basename $FILE)
+    if [ ! -f "$BASE_DIR/files/$FILENAME" ]; then
+	wget $FILE
+    fi
+done
+
+# jks is always created from the certificates
+if [ "$PROTOCOL" = "https" ]; then
+    if [ "$HTTPS_LETSENCRYPT" = "true" ]; then
+	HTTPS_CERT_PATH=/etc/letsencrypt/live/$BASE_DOMAIN
+        echo "Looking for SSL keys in $HTTPS_CERT_PATH..."
+	# If started by docker-compose, let's wait until certbot completes
+	until [ -f $HTTPS_CERT_PATH/$HTTPS_PRIVKEY ]; do
+            echo "Keys not found, waiting..."
+	    sleep 5
+        done
+    fi
+
+    openssl pkcs12 -export -out $TOMCAT_DIR/ssl/hmdm.p12 -inkey $HTTPS_CERT_PATH/$HTTPS_PRIVKEY -in $HTTPS_CERT_PATH/$HTTPS_CERT -certfile $HTTPS_CERT_PATH/$HTTPS_FULLCHAIN -password pass:$PASSWORD
+    keytool -importkeystore -destkeystore $TOMCAT_DIR/ssl/hmdm.jks -srckeystore $TOMCAT_DIR/ssl/hmdm.p12 -srcstoretype PKCS12 -srcstorepass $PASSWORD -deststorepass $PASSWORD -noprompt    
+fi
+
+# Waiting for the database
+until PGPASSWORD=$SQL_PASS psql -h "$SQL_HOST" -U "$SQL_USER" -p $SQL_PORT -d "$SQL_BASE" -c '\q'; do
+  echo "Waiting for the PostgreSQL database..."
+  sleep 5
+done
+
+# Avoid delays due to an issue with a random number
+cp /opt/java/openjdk/conf/security/java.security /tmp/java.security
+cat /tmp/java.security | sed "s|securerandom.source=file:/dev/random|securerandom.source=file:/dev/urandom|g" > /opt/java/openjdk/conf/security/java.security
+rm /tmp/java.security
+
+catalina.sh run
+
+#sleep 100000
